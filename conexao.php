1c01e20ffaefeb1d86ded19ed0f84e567217b010
email <isabelapaiola6@gmail.com>
2024-11-05 11:12:38 -0300

    Initial commit

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+    $servername = "localhost:3309";
+    $username = "root";
+    $password = "";
+    $dbname = "crudS";
+
+// Váriavel de conexão (todos os parâmetros do banco)
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn -> connect_error){
+    echo "Erro de conexão".$conn -> connect_error;
+} else{
+    echo "Conexão Bem-Sucedida!";
+}
+?>
